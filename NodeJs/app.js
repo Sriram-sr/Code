52baf2d59ae754fe437b55f96e033d3385c1e238
Sriram-sr <srirampanneer90@gmail.com>
2023-08-13 11:11:25 +0530

    Starting Node with Express

--- NodeJs/app.js
+++ NodeJs/app.js
@@ -1,24 +1,19 @@
-const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const errorController = require('./controllers/error');
+const adminRoutes = require('./routes/admin-routes');
+const shopRoutes = require('./routes/shop-routes');
+const rootDir = require('./utils/path');
+const path = require('path');
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(rootDir, 'public')));
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
-app.use(errorController.get404);
+app.use('/admin', adminRoutes);
+app.use((req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', '404.html'));
+});
 
 app.listen(3000);
---
+++ NodeJs/readme.md
@@ -0,0 +1,3 @@
+So far what has been done in coding part is,
+
+Made the express server up and running and html pages addded which is returned as responses to incoming requests.
