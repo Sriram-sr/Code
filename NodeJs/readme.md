88bc0d643c10cc9d246756f5a58c61e47a92e313
Sriram-sr <srirampanneer90@gmail.com>
2023-09-08 13:12:03 +0530

    Cleaning up Node files

--- NodeJs/readme.md
+++
@@ -1,3 +0,0 @@
-So far what has been done in coding part is,
-
-Finished how to use mysql in node js with Sequelize package and now onto MongoDB.
--- NodeJs/utils/path.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-
-exports.rootDir = path.dirname(require.main.filename); // This will return root directory
-exports.staticDir = path.join(this.rootDir, 'public');
-
-
