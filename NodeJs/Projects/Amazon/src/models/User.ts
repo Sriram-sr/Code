c55520fd6024b34af41d21d9f1aaed95fb47ae9c
Sriram-sr <srirampanneer90@gmail.com>
2024-01-10 18:30:05 +0530

    Added user model to kickstart amazon like backend

---
+++ NodeJs/Projects/Amazon/src/models/User.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from 'mongoose';
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  mobile: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+export default mongoose.model('User', userSchema);
