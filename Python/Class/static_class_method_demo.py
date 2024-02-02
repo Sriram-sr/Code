e3d6a6b7d9e94d4f7dcad2b467360be84bf50fa7
Sriram-sr <srirampanneer90@gmail.com>
2024-02-02 13:06:08 +0530

    python access parent class attribute

--- Python/Class/static_class_method_demo.py
+++ Python/Class/static_class_method_demo.py
@@ -10,6 +10,13 @@ class Base:
         print(f'Accessing class variable {cls.single_key}')
 
 
+class User(Base):
+    pass
+
+
 base_instance = Base()
 # base_instance.normal_static_method()
 Base.class_method_demo()
+
+print('Accessing parent class\'s \"class attribute\"...')
+print(User.single_key)
