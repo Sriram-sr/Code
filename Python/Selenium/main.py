f5cab5f53e90c43cdfe973e3f945a049d609ee1d
Sriram-sr <srirampanneer90@gmail.com>
2024-08-05 15:51:06 +0530

    Selenium - first login

--- Python/Selenium/main.py
+++ Python/Selenium/main.py
@@ -3,6 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 driver_path = r"D:\Git-Repos\Code\Python\Selenium\chromedriver.exe"
 service = Service(driver_path)
@@ -18,7 +19,12 @@ driver = webdriver.Chrome(service=service, options=options)
 
 try:
     driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-
+    print('Waiting 4 seconds for page to load fully...')
+    time.sleep(4)
+    driver.find_element(By.NAME, 'username').send_keys('Admin')
+    driver.find_element(By.NAME, 'password').send_keys('admin123')
+    driver.find_element(By.CLASS_NAME, 'oxd-button').click()
 finally:
+    print('Waiting 4 seconds before quitting the page...')
     time.sleep(4)
     driver.quit()
