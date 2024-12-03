7a0e7ce0fc15e844e18f4198ab4e8d7c0dcf39b2
Sriram-sr <srirampanneer90@gmail.com>
2024-12-03 12:29:23 +0530

    Programs - enhancement to palindrome code

--- Python/Class/Programs/logical_pgms.py
+++ Python/Class/Programs/logical_pgms.py
@@ -1,6 +1,8 @@
 # Palindrome Check
 
 def is_palindrome(text):
+    text = ''.join(char.lower() for char in text if char.isalnum())
+    print(text)
     return text.lower() == text.lower()[::-1]
 
-print(is_palindrome('Level'))
+print(is_palindrome("A man, a plan, a canal: Panama"))
