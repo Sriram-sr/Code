a2663f48e13a02b9ddad67b6698095c00fde9675
Sriram-sr <srirampanneer90@gmail.com>
2024-12-07 13:37:01 +0530

    Programs - factorial and prime programs

--- Python/Class/Programs/logical_pgms.py
+++ Python/Class/Programs/logical_pgms.py
@@ -8,6 +8,8 @@ def is_palindrome(text):
 # print(is_palindrome("A man, a plan, a canal: Panama"))
 
 
+# Fibonacci series
+
 def get_nth_fibonacci_num(pos):
     first_fibbo_num = 0
     second_fibbo_num = 1
@@ -42,5 +44,41 @@ def get_nth_fibonacci(pos):
     return second_num
 
 
-print(get_nth_fibonacci_num(40))
-print(get_nth_fibonacci(40))
+# print(get_nth_fibonacci_num(40))
+# print(get_nth_fibonacci(40))
+
+
+# Prime number
+
+def is_prime_number(num):
+    if num == 0 or num == 1:
+        return False
+    for div in range(2, num):
+        if num % div == 0:
+            return False
+    else:
+        return True
+
+
+# print(is_prime_number(2))
+
+
+# Factorial
+
+def find_factorial(num):
+    factorial = 1
+    for val in range(1, num + 1):
+        factorial *= val
+
+    return factorial
+
+
+def get_recursive_factorial(num):
+    if num == 1:
+        return 1
+
+    return num * get_recursive_factorial(num - 1)
+
+
+print(find_factorial(5))
+print(get_recursive_factorial(5))
