b7f338f4b12cb1a500890a2ea19e0a7333e8286b
Sriram-sr <srirampanneer90@gmail.com>
2025-01-13 13:24:30 +0530

    Programs - one approach to reverse string

--- JS/Programs/src/logical-pgms.ts
+++ JS/Programs/src/logical-pgms.ts
@@ -1,4 +1,7 @@
-// Leap year
+/**
+ * Program: Leap year
+ *
+ */
 
 function isLeapYear(year: number): boolean {
   if (year % 4 === 0) {
@@ -11,38 +14,43 @@ function isLeapYear(year: number): boolean {
   } else {
     return false;
   }
+
+  // Approach 2
+
+  // return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 }
 
 console.log(isLeapYear(1992));
 
-// Approach 2
-
-function isLeapyear(year: number): boolean {
-  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
-}
-
-// Reverse a string
+/**
+ * Program: Reverse a string
+ *
+ */
 
 function reverseString(str: string): string {
-  let reversedText = '';
+  // let reversedText = '';
 
-  for (let char of str) {
-    reversedText = char + reversedText;
+  // for (let char of str) {
+  //   reversedText = char + reversedText;
+  // }
+
+  // Approach 2
+
+  // for (let idx = str.length - 1; idx >= 0; idx--) {
+  //   reversedText += str[idx];
+  // }
+
+  // Approach 3
+  // reversedText = str.split('').reverse().join('');
+
+  // Approach 4
+  if (str === '') {
+    return '';
   }
 
-  return reversedText;
+  return reverseString(str.substring(1)) + str[0];
+
+  // return reversedText;
 }
 
-// Approach 2
-
-function reversesString(str: string): string {
-  let reversedText = '';
-
-  for (let idx = str.length - 1; idx >= 0; idx--) {
-    reversedText += str[idx];
-  }
-
-  return reversedText;
-}
-
-console.log(reversesString('hello'));
+console.log(reverseString('hello'));
