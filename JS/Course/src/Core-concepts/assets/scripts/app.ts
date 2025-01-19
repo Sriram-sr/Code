a69733a8ef45e92b2d1fc8b762960ba0800fd028
Sriram-sr <srirampanneer90@gmail.com>
2025-01-19 17:48:02 +0530

    JS - added reset functionality to the game

--- JS/Course/src/Core-concepts/assets/scripts/app.ts
+++ JS/Course/src/Core-concepts/assets/scripts/app.ts
@@ -5,18 +5,42 @@ const HEAL_VALUE = 20;
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
 
+function reset(): void {
+  currentMonsterHealth = chosenMaxLife;
+  currentPlayerHealth = chosenMaxLife;
+  resetGame(chosenMaxLife);
+}
+
 function endRound(): void {
+  let initialPlayerHealth = currentPlayerHealth;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
-  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+  if (currentPlayerHealth <= 0 && hasBonusLife) {
+    currentPlayerHealth = initialPlayerHealth;
+    hasBonusLife = false;
+    removeBonusLife();
+    setPlayerHealth(initialPlayerHealth);
+    alert('You would be lost but the bonus life saved you!');
+  }
+  const playerWinCondition =
+    currentMonsterHealth <= 0 && currentPlayerHealth > 0;
+  const monsterWinCondition =
+    currentPlayerHealth <= 0 && currentMonsterHealth > 0;
+  const drawCondition = currentMonsterHealth <= 0 && currentPlayerHealth <= 0;
+  if (playerWinCondition) {
     alert('Player Won!');
-  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+  } else if (monsterWinCondition) {
     alert('Monster Won!');
-  } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+  } else if (drawCondition) {
     alert('Game is draw!');
+  }
+
+  if (playerWinCondition || monsterWinCondition || drawCondition) {
+    reset();
   }
 }
 
@@ -41,8 +65,15 @@ function strongAttackHandler(): void {
 }
 
 function healPlayerHandler(): void {
-  increasePlayerHealth(HEAL_VALUE);
-  currentPlayerHealth += HEAL_VALUE;
+  let healValue: number;
+  if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
+    alert("You can't heal more than your choosen Max life");
+    healValue = chosenMaxLife - currentPlayerHealth;
+  } else {
+    healValue = HEAL_VALUE;
+  }
+  increasePlayerHealth(healValue);
+  currentPlayerHealth += healValue;
   endRound();
 }
 
