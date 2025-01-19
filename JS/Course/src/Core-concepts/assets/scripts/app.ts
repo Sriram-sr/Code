94a78dc341b2f92412c1ec8ea206c89678fed166
Sriram-sr <srirampanneer90@gmail.com>
2025-01-19 12:04:33 +0530

    JS - strong attack and heal functionalities added

--- JS/Course/src/Core-concepts/assets/scripts/app.ts
+++ JS/Course/src/Core-concepts/assets/scripts/app.ts
@@ -1,15 +1,15 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
+const HEAL_VALUE = 20;
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-  const monsterDamage = dealMonsterDamage(ATTACK_VALUE);
+function endRound(): void {
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-  currentMonsterHealth -= monsterDamage;
   currentPlayerHealth -= playerDamage;
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     alert('Player Won!');
@@ -20,4 +20,32 @@ function attackHandler() {
   }
 }
 
+function attackMonster(mode: 'ATTACK' | 'STONG_ATTACK'): void {
+  let maxDamage;
+  if (mode === 'ATTACK') {
+    maxDamage = ATTACK_VALUE;
+  } else {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  const monsterDamage = dealMonsterDamage(maxDamage);
+  currentMonsterHealth -= monsterDamage;
+  endRound();
+}
+
+function attackHandler(): void {
+  attackMonster('ATTACK');
+}
+
+function strongAttackHandler(): void {
+  attackMonster('STONG_ATTACK');
+}
+
+function healPlayerHandler(): void {
+  increasePlayerHealth(HEAL_VALUE);
+  currentPlayerHealth += HEAL_VALUE;
+  endRound();
+}
+
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healPlayerHandler);
