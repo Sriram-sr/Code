cda301b22c45e315668a84cffaa3d28e8bcf1fd7
Sriram-sr <srirampanneer90@gmail.com>
2025-02-03 12:24:41 +0530

    JS - adding working functionality for game

--- JS/Course/src/Functions/app.ts
+++ JS/Course/src/Functions/app.ts
@@ -2,23 +2,48 @@ const startGameBtn = document.getElementById(
   'start-game-btn'
 ) as HTMLButtonElement;
 
-const SELECTION_ROCK = 'rock';
-const SELECTION_PAPER = 'paper';
-const SELECTION_SCISSORS = 'scissors';
+const ROCK = 'rock';
+const PAPER = 'paper';
+const SCISSORS = 'scissors';
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
 let isGameRunning = false;
 
 const getPlayerChoice = function () {
   const selection = prompt('Rock, Paper or Scissors?', '')?.toLowerCase();
-  if (
-    selection !== SELECTION_ROCK &&
-    selection !== SELECTION_PAPER &&
-    selection !== SELECTION_SCISSORS
-  ) {
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
     alert('Invalid choice! We chose Rock for you!');
     return 'rock';
   }
   return selection;
+};
+
+const getComputerChoice = function () {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+const getWinner = function (cChoice: string, pChoice: string) {
+  console.log(`Computer chose ${cChoice}`);
+  if (cChoice === pChoice) {
+    return RESULT_DRAW;
+  } else if (
+    (cChoice === ROCK && pChoice === PAPER) ||
+    (cChoice === PAPER && pChoice === SCISSORS) ||
+    (cChoice === SCISSORS && pChoice === ROCK)
+  ) {
+    return RESULT_PLAYER_WINS;
+  } else {
+    return RESULT_COMPUTER_WINS;
+  }
 };
 
 startGameBtn.addEventListener('click', function () {
@@ -26,7 +51,8 @@ startGameBtn.addEventListener('click', function () {
     return;
   }
   isGameRunning = true;
-  console.log('Game is starting...');
+  const computerChoice = getComputerChoice();
   const playerChoice = getPlayerChoice();
-  console.log(playerChoice);
+  const winner = getWinner(computerChoice, playerChoice);
+  console.log(winner);
 });
