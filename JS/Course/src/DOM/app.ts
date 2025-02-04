2ea949b469444e9bc05fdb02d7ba4f7c9e644ea1
Sriram-sr <srirampanneer90@gmail.com>
2025-02-04 18:23:06 +0530

    JS - input element properties

--- JS/Course/src/DOM/app.ts
+++ JS/Course/src/DOM/app.ts
@@ -3,9 +3,12 @@ const listItems = document.getElementsByClassName('list-item');
 const allListItems = document.querySelectorAll('li');
 const firstListItem = document.querySelector('.list-item');
 const lastListItem = document.querySelector('ul :last-of-type');
+const inputElement = document.querySelector('input') as HTMLInputElement;
 
-console.log(h1Element);
-console.log(firstListItem);
-console.log(listItems);
-console.log(allListItems);
-console.log(lastListItem);
+h1Element.textContent = 'New text content!';
+h1Element.className = 'dynamic-inserted-class';
+h1Element.id = 'dynamic-inserted-id';
+h1Element.style.backgroundColor = '#ff1b68';
+
+inputElement.setAttribute('input', 'Modified using attribute');
+console.log(inputElement.getAttribute('input'));
