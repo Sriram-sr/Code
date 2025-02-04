5269b016f569586864dee3f2004a0b79f7e5adff
Sriram-sr <srirampanneer90@gmail.com>
2025-02-04 21:39:15 +0530

    JS - some more dom methods

--- JS/Course/src/DOM/app.ts
+++ JS/Course/src/DOM/app.ts
@@ -1,14 +1,31 @@
 const h1Element = document.getElementById('main-title') as HTMLHeadElement;
-const listItems = document.getElementsByClassName('list-item');
-const allListItems = document.querySelectorAll('li');
-const firstListItem = document.querySelector('.list-item');
-const lastListItem = document.querySelector('ul :last-of-type');
+const listItems = document.getElementsByClassName(
+  'list-item'
+) as HTMLCollectionOf<HTMLLIElement>;
+const allListItems = document.querySelectorAll(
+  'li'
+) as NodeListOf<HTMLLIElement>;
+// const firstListItem = document.querySelector('.list-item') as HTMLLIElement;
+const lastListItem = document.querySelector(
+  'ul :last-of-type'
+) as HTMLLIElement;
 const inputElement = document.querySelector('input') as HTMLInputElement;
+const titleElement = document.querySelector('title') as HTMLTitleElement;
+const unorderedList = document.querySelector('ul') as HTMLUListElement;
 
 h1Element.textContent = 'New text content!';
 h1Element.className = 'dynamic-inserted-class';
 h1Element.id = 'dynamic-inserted-id';
 h1Element.style.backgroundColor = '#ff1b68';
+titleElement.textContent = 'Assignment solved!';
 
 inputElement.setAttribute('input', 'Modified using attribute');
 console.log(inputElement.getAttribute('input'));
+
+const ulListChildren = unorderedList.children;
+const ulListChildNodes = unorderedList.childNodes;
+const firstListItem = unorderedList.firstChild; // this will be a text node
+const firstListItemElement = unorderedList.firstElementChild;
+
+console.log(firstListItem);
+console.log(firstListItemElement);
