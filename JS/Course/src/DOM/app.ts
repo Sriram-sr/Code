214c15ba33cb61211cd2c1f579f83c89dbccf254
Sriram-sr <srirampanneer90@gmail.com>
2025-02-06 16:46:10 +0530

    JS - adding new elements to DOM

--- JS/Course/src/DOM/app.ts
+++ JS/Course/src/DOM/app.ts
@@ -14,6 +14,7 @@ const titleElement = document.querySelector('title') as HTMLTitleElement;
 const unorderedList = document.querySelector('ul') as HTMLUListElement;
 const sectionElement = document.querySelector('section') as HTMLElement;
 const button = document.querySelector('button') as HTMLButtonElement;
+const specialList = document.querySelector('.special-list') as HTMLUListElement;
 
 h1Element.textContent = 'New text content!';
 h1Element.className = 'dynamic-inserted-class';
@@ -38,3 +39,16 @@ const inputElementBySibling = unorderedList.nextElementSibling;
 button?.addEventListener('click', () => {
   sectionElement.classList.toggle('invisible');
 });
+
+// Creating new Html elements
+
+// specialList.innerHTML = '<h1>Modified the list with H1'; // Replaces the entire list content with H1
+const lastSpecialListItem = specialList.lastElementChild;
+lastSpecialListItem?.insertAdjacentHTML(
+  'afterend',
+  '<li>Before special last item</li>'
+);
+const dynamicListItem = document.createElement('li') as HTMLLIElement;
+
+specialList.appendChild(dynamicListItem);
+dynamicListItem.textContent = 'Dynamically added list item';
