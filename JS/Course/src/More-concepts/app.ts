d848190f5184db8af6b4aaa81a3db47e44bba986
Sriram-sr <srirampanneer90@gmail.com>
2025-02-19 22:36:44 +0530

    JS - some more array methods

--- JS/Course/src/More-concepts/app.ts
+++ JS/Course/src/More-concepts/app.ts
@@ -1,4 +1,14 @@
+// Array methods
+
 const nodeListItems = document.querySelectorAll('li');
-console.log(nodeListItems);
 const arrayListItems = Array.from(nodeListItems);
-console.log(arrayListItems);
+
+const hobbies = ['Music', 'Coding'];
+
+hobbies.unshift('Sports');
+hobbies.splice(1, 1, 'Computing', 'Developing');
+hobbies.splice(0, 0, 'Music');
+hobbies.splice(0, 1); // Deletes one element
+hobbies.shift();
+
+const realCopiedArray = hobbies.slice();
