694cafaa17c81b473ac8c68c390fc3825d3e9ac6
Sriram-sr <srirampanneer90@gmail.com>
2025-02-20 12:18:42 +0530

    JS - some more array functions

--- JS/Course/src/More-concepts/app.ts
+++ JS/Course/src/More-concepts/app.ts
@@ -4,6 +4,7 @@ const nodeListItems = document.querySelectorAll('li');
 const arrayListItems = Array.from(nodeListItems);
 
 const hobbies = ['Music', 'Coding'];
+const tasks = ['Do Code', 'Do compute'];
 
 hobbies.unshift('Sports');
 hobbies.splice(1, 1, 'Computing', 'Developing');
@@ -12,3 +13,11 @@ hobbies.splice(0, 1); // Deletes one element
 hobbies.shift();
 
 const realCopiedArray = hobbies.slice();
+const concatenatedArray = hobbies.concat(tasks);
+
+const numbers = [2, 3, 4, 9, 2, 11];
+const indexOfTwo = numbers.indexOf(2);
+const indexOfTwoFromReverse = numbers.lastIndexOf(2);
+const foundValue = numbers.find((number, _idx, _allNumbers) => {
+  return number == 9;
+});
