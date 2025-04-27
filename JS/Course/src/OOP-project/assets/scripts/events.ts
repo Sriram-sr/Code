1d5eb21f84fb970c5220a527f5e2bf394748c309
Sriram-sr <srirampanneer90@gmail.com>
2025-04-27 20:32:32 +0530

    JS - more events with button

--- JS/Course/src/OOP-project/assets/scripts/events.ts
+++ JS/Course/src/OOP-project/assets/scripts/events.ts
@@ -1,7 +1,16 @@
 const buttonElement = document.querySelector('button') as HTMLButtonElement;
 
-const buttonClickHandler = () => {
+const buttonClickHandler = (event: MouseEvent) => {
   console.log('Button is clicked...');
+  console.log(event);
+  // event.target.disabled = true; // Event.target gives exact element on which event is triggered
 };
 
-buttonElement.onclick = buttonClickHandler;
+// buttonElement.onclick = buttonClickHandler;
+
+// buttonElement.addEventListener('click', buttonClickHandler);
+buttonElement.addEventListener('mouseenter', buttonClickHandler);
+
+// setTimeout(() => {
+//   buttonElement.removeEventListener('click', buttonClickHandler);
+// }, 2000);
