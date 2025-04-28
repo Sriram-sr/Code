adf3432b5f84c5316c70dcd778db379865a3ed2e
Sriram-sr <srirampanneer90@gmail.com>
2025-04-28 17:57:38 +0530

    JS - eventlisteners and propagations

--- JS/Course/src/OOP-project/assets/scripts/events.ts
+++ JS/Course/src/OOP-project/assets/scripts/events.ts
@@ -1,6 +1,7 @@
 const buttonElement = document.querySelector('button') as HTMLButtonElement;
 
 const buttonClickHandler = (event: MouseEvent) => {
+  event.stopPropagation(); // Stops the listeners registered on any parent elements
   console.log('Button is clicked...');
   console.log(event);
   // event.target.disabled = true; // Event.target gives exact element on which event is triggered
@@ -8,9 +9,24 @@ const buttonClickHandler = (event: MouseEvent) => {
 
 // buttonElement.onclick = buttonClickHandler;
 
-// buttonElement.addEventListener('click', buttonClickHandler);
-buttonElement.addEventListener('mouseenter', buttonClickHandler);
+buttonElement.addEventListener('click', buttonClickHandler);
+// buttonElement.addEventListener('mouseenter', buttonClickHandler);
 
 // setTimeout(() => {
 //   buttonElement.removeEventListener('click', buttonClickHandler);
 // }, 2000);
+
+const form = document.querySelector('form');
+form?.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(event);
+});
+
+const div = document.querySelector('div');
+div?.addEventListener(
+  'click',
+  () => {
+    console.log('Div is clicked!!!');
+  }
+  // true // To make the Div eventlistener to execute on capture phase before button event listener
+);
