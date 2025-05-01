9fb57edc08c83ff296a0943808fd23d1ba316216
Sriram-sr <srirampanneer90@gmail.com>
2025-05-01 21:48:30 +0530

    JS - event delegation concept

--- JS/Course/src/OOP-project/assets/scripts/events.ts
+++ JS/Course/src/OOP-project/assets/scripts/events.ts
@@ -1,9 +1,15 @@
-const buttonElement = document.querySelector('button') as HTMLButtonElement;
+const buttonElement = document.querySelector(
+  '#generic-button'
+) as HTMLButtonElement;
+const submitButton = document.querySelector(
+  '#submit-button'
+) as HTMLButtonElement;
 
 const buttonClickHandler = (event: MouseEvent) => {
   event.stopPropagation(); // Stops the listeners registered on any parent elements
   console.log('Button is clicked...');
   console.log(event);
+  // console.log(this); //  Will be the event target (button in this case)
   // event.target.disabled = true; // Event.target gives exact element on which event is triggered
 };
 
@@ -30,3 +36,13 @@ div?.addEventListener(
   }
   // true // To make the Div eventlistener to execute on capture phase before button event listener
 );
+
+const listElement = document.querySelector('ul');
+
+listElement?.addEventListener('click', event => {
+  const targetElement = event.target as HTMLElement;
+  targetElement.closest('li')?.classList.toggle('highlight');
+  console.log(event.currentTarget); // Always be ul where we added the lisetener
+  // form?.submit();
+  submitButton.click();
+});
