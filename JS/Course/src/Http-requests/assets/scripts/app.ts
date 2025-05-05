2fbe7a0c26c0476cf6c9c87687079219b9d7d92a
Sriram-sr <srirampanneer90@gmail.com>
2025-05-05 22:28:17 +0530

    JS - rendering fetched posts

--- JS/Course/src/Http-requests/assets/scripts/app.ts
+++ JS/Course/src/Http-requests/assets/scripts/app.ts
@@ -1,1 +1,26 @@
+const ulListElement = document.querySelector('.posts') as HTMLUListElement;
+const postTemplate = document.getElementById(
+  'single-post'
+) as HTMLTemplateElement;
+
 const xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+xhr.onload = function () {
+  let listOfPosts: Array<Post> = JSON.parse(xhr.response).slice(0, 10);
+  for (const post of listOfPosts) {
+    const postElement = document.importNode(postTemplate.content, true);
+    postElement.querySelector('h2')!.textContent = post.title.toUpperCase();
+    postElement.querySelector('p')!.textContent = post.body;
+    ulListElement.appendChild(postElement);
+  }
+};
+
+xhr.send();
