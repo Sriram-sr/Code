009eafbd7b954d87da997bc352f877ef8ee772f0
Sriram-sr <srirampanneer90@gmail.com>
2025-05-06 00:00:27 +0530

    JS - changing fetch methods as promise

--- JS/Course/src/Http-requests/assets/scripts/app.ts
+++ JS/Course/src/Http-requests/assets/scripts/app.ts
@@ -3,9 +3,6 @@ const postTemplate = document.getElementById(
   'single-post'
 ) as HTMLTemplateElement;
 
-const xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
-
 interface Post {
   userId: number;
   id: number;
@@ -13,14 +10,38 @@ interface Post {
   body: string;
 }
 
-xhr.onload = function () {
-  let listOfPosts: Array<Post> = JSON.parse(xhr.response).slice(0, 10);
-  for (const post of listOfPosts) {
-    const postElement = document.importNode(postTemplate.content, true);
-    postElement.querySelector('h2')!.textContent = post.title.toUpperCase();
-    postElement.querySelector('p')!.textContent = post.body;
-    ulListElement.appendChild(postElement);
-  }
+const sendHttpRequest: (method: string, url: string) => Promise<string> = (
+  method: string,
+  url: string
+) => {
+  const promise = new Promise<string>((resolve, _reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+
+    xhr.onload = function () {
+      resolve(xhr.response);
+    };
+
+    xhr.send();
+  });
+
+  return promise;
 };
 
-xhr.send();
+const fetchPosts: () => void = () => {
+  sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts')
+    .then((data: string) => {
+      let listOfPosts: Array<Post> = JSON.parse(data).slice(0, 10);
+      for (const post of listOfPosts) {
+        const postElement = document.importNode(postTemplate.content, true);
+        postElement.querySelector('h2')!.textContent = post.title.toUpperCase();
+        postElement.querySelector('p')!.textContent = post.body;
+        ulListElement.appendChild(postElement);
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+fetchPosts();
