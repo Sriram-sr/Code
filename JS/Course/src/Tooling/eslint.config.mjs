966998ec8473020c0c2642a8c00d85c3a81e831f
Sriram-sr <srirampanneer90@gmail.com>
2025-05-12 18:33:56 +0530

    JS - adding basic eslint configs

--- JS/Course/src/Tooling/eslint.config.mjs
+++ JS/Course/src/Tooling/eslint.config.mjs
@@ -2,8 +2,28 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    rules: {
+      'no-console': 'warn',
+      'no-unused-vars': 'warn',
+      'no-debugger': 'error',
+      'no-dupe-keys': 'error',
+      'no-empty': 'warn',
+      'no-extra-semi': 'warn',
+      'no-irregular-whitespace': 'error',
+      'no-undef': 'error',
+      'no-unreachable': 'error',
+      'valid-typeof': 'error',
+      'semi': 'error',
+      // 'newline-after-var': ['error', 'always']
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: { globals: globals.browser },
+  },
 ]);
