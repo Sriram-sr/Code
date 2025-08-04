9ed2243c72d08f615369dd84aa0245326c87517b
Sriram-sr <srirampanneer90@gmail.com>
2025-08-04 13:18:23 +0530

    Java - variables

--- Java/Hello.java
+++ Java/Hello.java
@@ -1,5 +1,15 @@
 public class Hello {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("Hello World");
+        
+		// Variables
+		
+		final String constant = "Constant variable";
+		String stringVar = "String data type";
+		int intVar = 8;
+		float floatVar = 7.6f;
+		boolean boolVar = false;
+		char charVar = 'k';
+		System.out.println(constant + " " + stringVar + " " + intVar + " " + floatVar + " " + boolVar + " " + charVar);
 	}
 }
