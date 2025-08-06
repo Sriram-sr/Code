30610fcf7420d14cd2b85b35b96215912d39e530
Sriram-sr <srirampanneer90@gmail.com>
2025-08-06 18:40:53 +0530

    Java - more concepts

--- Java/Hello.java
+++ Java/Hello.java
@@ -1,3 +1,9 @@
+class Calculator {
+	public int addition(int num1, int num2) {
+		return num1 + num2;
+	}
+}
+
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
@@ -14,5 +20,35 @@ public class Hello {
 		int doubToIntVar = (int) doubVar; // Type casting
 		System.out.println(constant + " " + stringVar + " " + intVar + " " + floatVar + " " + boolVar + " " + charVar
 				+ " " + doubToIntVar);
+
+		// Switch statement
+
+		int weekDay = 1;
+		switch (weekDay) {
+			case 1:
+				System.out.println("Monday");
+				break;
+			case 2:
+				System.out.println("Tuesday");
+				break;
+			case 3:
+				System.out.println("Wednesday");
+				break;
+			default:
+				System.out.println("Enter a valid day!");
+				break;
+		}
+
+		// Arrays
+
+		int[] numbers = { 1, 2, 3, 4, 5 };
+		System.out.println(numbers.length);
+		System.out.println(numbers[2]);
+
+		// Objects
+
+		Calculator calc = new Calculator();
+		int addOutput = calc.addition(4, 5);
+		System.out.println(addOutput);
 	}
 }
