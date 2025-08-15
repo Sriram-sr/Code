ee255910c02515e541151bd6cd0dfa10fd16964b
Sriram-sr <srirampanneer90@gmail.com>
2025-08-15 13:36:49 +0530

    Java - couple of programs

--- Java/Concepts/Programs.java
+++ Java/Concepts/Programs.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Programs {
     public static void main(String args[]) {
         // Odd or Even
@@ -27,5 +30,38 @@ public class Programs {
             number /= 10;
         }
         System.out.println(reversed);
+
+        // Greatest of three numbers
+
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+
+        if (num1 > num2 && num1 > num3) {
+            System.out.println("Num 1 is the greatest");
+        } else if (num2 > num1 && num2 > num3) {
+            System.out.println("Num 2 is the greatest");
+        } else if (num3 > num1 && num3 > num2) {
+            System.out.println("Num 3 is the greatest");
+        } else {
+            System.out.println("No number is the greatest");
+        }
+
+        // Sum of elements
+
+        int size = scanner.nextInt();
+        int[] elements = new int[size];
+        int sum = 0;
+
+        for (int idx = 0; idx < size; idx++) {
+            elements[idx] = scanner.nextInt();
+            sum += elements[idx];
+        }
+
+        System.out.println(Arrays.toString(elements));
+        System.out.println("Sum: " + sum);
+
+        scanner.close();
     }
 }
