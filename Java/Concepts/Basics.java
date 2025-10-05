ee11fc845faa95ae73808c34cff34dce198c17e4
Sriram-sr <srirampanneer90@gmail.com>
2025-10-06 00:34:59 +0530

    Java - Exceptions and threading

--- Java/Concepts/Basics.java
+++ Java/Concepts/Basics.java
@@ -4,26 +4,25 @@ import java.util.Scanner;
 class Employee {
     private String name;
     private double salary;
-    
+
     public Employee(String name, double salary) {
         this.name = name;
         this.salary = salary;
     }
-    
+
     public String getDetails() {
         return "Name: " + this.name + ", Salary: " + this.salary;
     }
 }
 
-
 class Manager extends Employee {
     private String department;
-    
+
     public Manager(String name, double salary, String department) {
         super(name, salary);
         this.department = department;
     }
-    
+
     public String getDetails() {
         return super.getDetails() + ", Department: " + this.department;
     }
@@ -32,6 +31,28 @@ class Manager extends Employee {
 enum StatusCodes {
     Failed, Passed, Skipped, Errored
 };
+
+class NegativeValueException extends Exception {
+    public NegativeValueException(String message) {
+        super(message);
+    }
+}
+
+class LearnOS extends Thread {
+    public void run() {
+        for (int cnt = 0; cnt < 100; cnt++) {
+            System.out.println("Learning operating system");
+        }
+    }
+}
+
+class LearnDSA extends Thread {
+    public void run() {
+        for (int cnt = 0; cnt < 100; cnt++) {
+            System.out.println("Learning Data Structures and Algorithms");
+        }
+    }
+}
 
 public class Basics {
     public static void main(String[] args) {
@@ -56,5 +77,27 @@ public class Basics {
         System.out.println(status);
         System.out.println(status.ordinal());
         System.out.println(Arrays.toString(allStatus));
+
+        int numerator = 50;
+        int denominator = -5;
+
+        try {
+            if (denominator < 0) {
+                throw new NegativeValueException("Denominator cannot be negative.");
+            }
+            int result = numerator / denominator;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Division by zero is not allowed.");
+        } catch (NegativeValueException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        // Multithreading
+
+        LearnOS osThread = new LearnOS();
+        LearnDSA dsaThread = new LearnDSA();
+        osThread.start();
+        dsaThread.start();
     }
 }
