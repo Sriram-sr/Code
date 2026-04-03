19dab05cde5eeb8802e0e90523df093f37409157
Sriram Panneerselvam <sriram@Srirams-MacBook-Pro.local>
2026-04-03 17:21:39 +0530

    Programs - finding insert position

--- Python/Class/Programs/cp_problems.py
+++ Python/Class/Programs/cp_problems.py
@@ -5,8 +5,34 @@ class Solution:
                 arr[idx], arr[idx - 1] = arr[idx - 1], arr[idx]
 
         return arr
+    
+    def find_insert_position(self, arr: list, k: int) -> int:
+        # for idx in range(len(arr)):
+        #     if arr[idx] == k:
+        #         return idx
+        #     elif arr[idx] > k:
+        #         return idx
+            
+        # return len(arr)
+
+        # Approach 2: Binary Search
+
+        low = 0
+        high = len(arr) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if arr[mid] == k:
+                return mid
+            elif arr[mid] < k:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return low
 
 
 solution = Solution()
-arr = [20, 10, 40, 30, 60, 50]
-print(solution.swap_adjacent(arr))
+arr = [1, 3, 5, 6]
+print(solution.find_insert_position(arr, 2))
