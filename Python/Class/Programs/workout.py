fb220311c532274f05a1ce77c91cb8eb043f6e73
Sriram Panneerselvam <sriram@Srirams-MacBook-Pro.local>
2026-04-18 20:36:05 +0530

    Programs - decoraters in python

--- Python/Class/Programs/workout.py
+++ Python/Class/Programs/workout.py
@@ -187,3 +187,19 @@ def get_username(cec_id):
 
 
 # get_username('sripanne')
+
+
+def calculate_sqaure(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+
+        return result ** 2
+    
+    return wrapper
+
+
+@calculate_sqaure
+def get_number(n):
+    return n
+
+print(get_number(2))
